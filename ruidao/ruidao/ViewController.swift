97aaab2a22d34bdcc968ba35e2b80ce6be8d3cb1
13110100523 <a13110100523@kanzakideimac.local>
2016-04-01 10:11:57 +0800

    hs

---
+++ ruidao/ruidao/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ruidao
+//
+//  Created by 13110100523 on 16/4/1.
+//  Copyright © 2016年 13110100523. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
